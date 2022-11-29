a8c80293bf61d4d6eccbd56074a9c26ea0e36edb
TeHb19 <53332538+TeHb19@users.noreply.github.com>
2022-11-29 08:48:36 +0600

    Add files via upload

---
+++ bad.py
@@ -0,0 +1,54 @@
+import numpy as np
+from PIL import Image
+import time
+
+#used characters
+chars=['.',',',';','*','+','%','#','$','&','@']
+
+
+def draw(name):
+    
+    #get rid of red and blue values
+    img=np.array(Image.open(name+'.bmp')).reshape(360,480,3)[:,:,1]
+
+
+    #current frame in ASCII
+    frame=''
+    
+    for i in range(60):
+   
+        row=''
+   
+        for j in range(160):
+    
+            value=np.sum(img[i*6:i*6+6,j*3:j*3+3])
+
+            #convert 3x6 region into a single ASCII character
+            row+=chars[int(value/460)]
+        
+        frame+=(row+'\n')
+    
+    return frame[:-1]
+
+
+frames=[""]*6567
+
+st=time.time()
+for i in range(1,6568):
+        name=str(i)
+        
+        if len(name)<3:
+        
+            name='0'*(3-len(name))+name
+       
+        frames[i-1]=draw(name)
+        print("Finished frame "+str(i)+" / 6567. Total time elapsed: " +str(time.time()-st)+"s")
+
+input("Press enter to continue")
+
+for frame in frames:
+        a=time.time()
+        print(frame)
+        b=time.time()-a
+        #change 1/15 to 1/30 for playback at 1x instead of 2x speed
+        time.sleep(1/15-b)
